5af2fc8520ed07693df83dcf4068482d9ea386d8
James Lambie <jameslambie@gmail.com>
2018-03-17 17:05:29 +0000

    Initial commit

---
+++ convert.js
@@ -0,0 +1,181 @@
+var xml2js = require('xml2js')
+var fs = require('fs')
+var path = require('path')
+var util = require('util')
+// var toMarkdown = require('to-markdown');
+var mkdirp = require('mkdirp')
+var TurndownService = require('turndown') // for Node.js
+var turndownService = new TurndownService()
+var http = require('http')
+
+processExport()
+
+function processExport () {
+  var parser = new xml2js.Parser()
+  fs.readFile('export.xml', function (err, data) {
+    if (err) {
+      console.log('Error: ' + err)
+    }
+
+	    parser.parseString(data, function (err, result) {
+	    	if (err) {
+	    		console.log('Error parsing xml: ' + err)
+	    	}
+	    	console.log('Parsed XML')
+	        // console.log(util.inspect(result.rss.channel));
+
+	        var posts = result.rss.channel[0].item
+
+      fs.mkdir('out', function () {
+		        for (var i = 0; i < posts.length; i++) {
+	        		processPost(posts[i])
+		        	// console.log(util.inspect(posts[i]));
+		        }
+      })
+	    })
+  })
+}
+
+function processPost (post) {
+  console.log('Processing Post')
+
+  console.log(`${post['wp:post_name']} ${post['wp:post_name'][0].length} \n`)
+  var postTitle = post.title
+  console.log('Post title: ' + postTitle)
+  var postDate = new Date(post.pubDate)
+  console.log('Post Date: ' + postDate)
+  var postData = post['content:encoded'][0]
+  console.log('Post length: ' + postData.length + ' bytes')
+  var slug = post['wp:post_name'][0] === '' ? 'index' : post['wp:post_name'][0]
+  console.log('Post slug: ' + slug)
+
+	// Merge categories and tags into tags
+  var categories = []
+  if (post.category != undefined) {
+    for (var i = 0; i < post.category.length; i++) {
+      var cat = post.category[i]['_']
+      if (cat != 'Uncategorized') { categories.push(cat) }
+			// console.log('CATEGORY: ' + util.inspect(post.category[i]['_']));
+    }
+  }
+
+  var fullPath = path.join('out', postDate.getFullYear().toString(), getPaddedMonthNumber(postDate.getMonth() + 1))  // 'out\\' + postDate.getFullYear() + '\\' + getPaddedMonthNumber(postDate.getMonth() + 1) + '\\' + slug
+
+  console.log(fullPath)
+  mkdirp(path.join('out', postDate.getFullYear().toString(), getPaddedMonthNumber(postDate.getMonth() + 1)), (err, made) => {
+  	console.log(err, made)
+  // fs.mkdir('out\\' + postDate.getFullYear(), function () {
+    // fs.mkdir('out\\' + postDate.getFullYear() + '\\' + getPaddedMonthNumber(postDate.getMonth() + 1), function () {
+      // fs.mkdir(fullPath, function () {
+				// Find all images
+    var patt = new RegExp('(?:src="(.*?)")', 'gi')
+
+    var m
+    var matches = []
+    while ((m = patt.exec(postData)) !== null) {
+      matches.push(m[1])
+					// console.log("Found: " + m[1]);
+    }
+
+    if (matches != null && matches.length > 0) {
+      for (var i = 0; i < matches.length; i++) {
+						// console.log('Post image found: ' + matches[i])
+
+        var url = matches[i]
+        var urlParts = matches[i].split('/')
+        var imageName = urlParts[urlParts.length - 1]
+
+        var filePath = path.join(fullPath, imageName)
+
+        downloadFile(url, filePath)
+
+						// Make the image name local relative in the markdown
+        postData = postData.replace(url, imageName)
+						// console.log('Replacing ' + url + ' with ' + imageName);
+      }
+    }
+
+    var markdown = turndownService.turndown(postData, { gfm: true })
+
+				// Fix characters that markdown doesn't like
+				// smart single quotes and apostrophe
+  			markdown = markdown.replace(/[\u2018|\u2019|\u201A]/g, "\'")
+  			// smart double quotes
+  			markdown = markdown.replace(/&quot;/g, '"')
+  			markdown = markdown.replace(/[\u201C|\u201D|\u201E]/g, '"')
+				// ellipsis
+    markdown = markdown.replace(/\u2026/g, '...')
+				// dashes
+    markdown = markdown.replace(/[\u2013|\u2014]/g, '-')
+				// circumflex
+    markdown = markdown.replace(/\u02C6/g, '^')
+				// open angle bracket
+    markdown = markdown.replace(/\u2039/g, '<')
+    markdown = markdown.replace(/&lt;/g, '<')
+				// close angle bracket
+    markdown = markdown.replace(/\u203A/g, '>')
+    markdown = markdown.replace(/&gt;/g, '>')
+				// spaces
+    markdown = markdown.replace(/[\u02DC|\u00A0]/g, ' ')
+				// ampersand
+    markdown = markdown.replace(/&amp;/g, '&')
+
+    var header = ''
+    header += '---\n'
+    header += 'layout: post\n'
+    header += 'title: ' + postTitle + '\n'
+    header += 'date: ' + postDate.getFullYear() + '-' + getPaddedMonthNumber(postDate.getMonth() + 1) + '-' + getPaddedDayNumber(postDate.getDate()) + '\n'
+    if (categories.length > 0) { header += 'tags: ' + JSON.stringify(categories) + '\n' }
+    header += '---\n'
+    header += '\n'
+
+    console.log(fullPath, slug)
+
+    fs.writeFile(path.join(fullPath, slug) + '.md', header + markdown, function (err) {
+
+    })
+  })
+
+  //     })
+  //   })
+  // })
+}
+
+function downloadFile (url, path) {
+	 console.log('Attempt downloading ' + url + ' to ' + path + ' ' + url.indexOf('https:'))
+  if (url.indexOf('https:') == -1) {
+    if (url.indexOf('.jpg') >= 0 || url.indexOf('.png') >= 0 || url.indexOf('.png') >= 0) {
+      var file = fs.createWriteStream(path).on('open', function () {
+        var request = http.get(url, function (response) {
+          console.log('Response code: ' + response.statusCode)
+          response.pipe(file)
+        }).on('error', function (err) {
+          console.log('error downloading url: ' + url + ' to ' + path)
+        })
+      }).on('error', function (err) {
+        console.log('error downloading url2: ' + url + ' to ' + path)
+      })
+    }	else {
+	  console.log('passing on: ' + url + ' ' + url.indexOf('https:'))
+    }
+  }	else {
+	  console.log('passing on: ' + url + ' ' + url.indexOf('https:'))
+  }
+}
+
+function getPaddedMonthNumber (month) {
+  console.log('month', month)
+  if (month < 10) {
+  	return '0' + month
+  } else {
+  	return '' + month
+  }
+}
+
+function getPaddedDayNumber (day) {
+  if (day < 10) {
+  	return '0' + day
+  } else {
+  	return '' + day
+  }
+}
